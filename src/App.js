eba8b1e43ffcb65e481c5217d4ee69047e69cdf9
Anton Shcherbakov <1mailanton@gmail.com>
2026-09-25 12:43:50 +0200

    added file retrieval via input and withdrawal file data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const [file, setFile] = useState([]);
+
+    function handleFileChange(event) {
+        const selectedFile = event.target.files[0];
+
+        setFile(selectedFile);
+    }
+
+    return (
+        <div className="App">
+            <input type="file" onChange={handleFileChange}/>
+            {file &&
+                <>
+                    <h1>name: {file.name}</h1>
+                    <h1>type: {file.type}</h1>
+                    <h1>size: {file.size}</h1>
+                </>
+            }
+        </div>
+    );
 }
 
 export default App;
